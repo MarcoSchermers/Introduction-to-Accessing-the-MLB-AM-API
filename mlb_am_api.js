470e44be8db03678f220d0a3fcbbbdcc03ec2cb5
michealwillard <willardm@onid.oregonstate.edu>
2015-02-27 13:29:22 -0800

    Completed index, section1, section2, example and mlb_am_api.js

--- mlb_am_api.js
+++ mlb_am_api.js
@@ -1,60 +1,111 @@
 http://www.w3schools.com/json/json_http.asp
 
-window.gameArray = [];
-
-function Game(sName, minTemp, maxTemp, rainMax) {
-  this.homeTeam = homeTeam;
-  this.awayTeam = awayTeam;
-  this.homeScore = homeScore;
-  this.awayScore = awayScore;
-  // Getters
-  this.setHomeTeam = function() {
+function Game() {
+  this.homeTeam = '';
+  this.awayTeam = '';
+  this.homeScore = '';
+  this.awayScore = '';
+  // Setters
+  this.setHomeTeam = function(homeTeam) {
     this.homeTeam = homeTeam;
   };
-  this.setAwayTeam = function() {
+  this.setAwayTeam = function(awayTeam) {
     this.awayTeam = awayTeam;
   };
-  this.setHomeScore = function() {
+  this.setHomeScore = function(homeScore) {
     this.homeScore = homeScore;
   };
-  this.setAwayScore = function() {
+  this.setAwayScore = function(awayScore) {
     this.awayScore = awayScore;
   };
-  // Setters
-  this.getHomeTeam = function() {
+  // Getters
+  this.getHomeTeam = function(homeTeam) {
     return this.homeTeam;
   };
-  this.getAwayTeam = function() {
+  this.getAwayTeam = function(awayTeam) {
     return this.awayTeam;
   };
-  this.getHomeScore = function() {
+  this.getHomeScore = function(homeScore) {
     return this.homeScore;
   };
-  this.getAwayScore = function() {
+  this.getAwayScore = function(awayScore) {
     return this.awayScore;
   };
 }
 
+function createScoreboard(scoreTable, response) {
+  console.log('Create Scoreboard Called');
+  var obj = response.data.games.game;
+  for (var key in obj) {
+      var tempItem = new Game();
+      // Set a 'no games message here'?
+      tempItem.setHomeTeam(obj[key].home_team_name);
+      tempItem.setAwayTeam(obj[key].away_team_name);
+      tempItem.setHomeScore(obj[key].home_team_runs);
+      tempItem.setAwayScore(obj[key].away_team_runs);
+      tableObject(scoreTable, tempItem);
+  }
+}
 
-function getTomorrowWeather() {
+function tableObject(scoreTable, tempItem) {
+  var newTable = document.createElement('table');
+  var tblBody = document.createElement('tbody');
+
+  // Build the Away Team Name and Score
+  var row1 = document.createElement('tr');
+
+  var th1 = document.createElement('th');
+  var th1Text = document.createTextNode(tempItem.getAwayTeam());
+  th1.appendChild(th1Text);
+  row1.appendChild(th1);
+
+  var td1 = document.createElement('td');
+  var td1Text = document.createTextNode(tempItem.getAwayScore());
+  td1.appendChild(td1Text);
+  row1.appendChild(td1);
+  tblBody.appendChild(row1);
+
+  // Build the Home Team Name and Score
+  var row2 = document.createElement('tr');
+
+  var th2 = document.createElement('th');
+  var th2Text = document.createTextNode(tempItem.getHomeTeam());
+  th2.appendChild(th2Text);
+  row2.appendChild(th2);
+
+  var td2 = document.createElement('td');
+  var td2Text = document.createTextNode(tempItem.getHomeScore());
+  td2.appendChild(td2Text);
+  row2.appendChild(td2);
+  tblBody.appendChild(row2);
+
+  newTable.appendChild(tblBody);
+  scoreTable.appendChild(newTable);
+  newTable.setAttribute("class", "table table-bordered");
+}
+
+function getScores() {
   var req = new XMLHttpRequest();
   if(!req) {
     throw 'Unable to create HttpRequest.';
   }
-  var yearSelect = document.getElementsByName('year-select')[0];
+  var yearSelect = document.getElementsByName('year')[0].value;
+  var monthSelect = document.getElementsByName('month')[0].value;
+  var daySelect = document.getElementsByName('day')[0].value;
   var url = 'http://gd2.mlb.com/components/game/mlb/';
-  var params = {
-    q: citySelect.option[citySelect.selectedIndex].value,
-    mode: json,
-    units: 'imperial',
-    cnt: '7'
-  };
-  url += '?' + urlStringify(params);
+  url += 'year_' + yearSelect + '/month_' + monthSelect + '/day_' + daySelect + '/miniscoreboard.json';
   req.onreadystatechange = function() {
     if(this.readyState === 4) {
-      console.log(this.responseText);
+      if(this.status === 200) {
+        var response = JSON.parse(this.responseText);
+        createScoreboard(document.getElementById('scoreboard'), response);
+      }
     }
   };
   req.open('GET', url);
   req.send();
+}
+
+window.onload = function() {
+  document.getElementById('scoreboard').innerHTML = '';
 }
